f312882401bda31795c09f226511b561bd669093
qifanyang <584874132@qq.com>
2014-10-11 18:12:48 +0800

    添加redis的master和slave测试

--- tool/src/test/java/jedis/test/RedisTest.java
+++ tool/src/test/java/jedis/test/RedisTest.java
@@ -11,6 +11,8 @@ public class RedisTest {
 		//redis-server redis.conf启动服务器
 		//在windows下启动指定参数文件 , 用来修改参数
 		//可以改端口,密码认证....
+		//requirepass foobared 指定redis密码
+		//masterauth  配置主从时,如果master设置了密码,这里写master的requirepass密码, 用于slave连接master
 		//默认端口6379
 		Jedis jedis = new Jedis("localhost", 6379);
 		jedis.auth("foobared");
@@ -24,9 +26,16 @@ public class RedisTest {
 		System.out.println(r);
 		
 		
-		
 		String value = jedis.get("foo");
 		System.out.println("redis value = " + value);
+		
+		//写到master,从slave中读取数据
+		jedis.set("masterv", "hahahgggggggggggggggggggggggggh");
+		
+		Jedis s1 = new Jedis("localhost", 7001);
+		s1.auth("foobared");
+		
+		System.out.println("s1 get master = " + s1.get("masterv"));
 	}
 
 }
