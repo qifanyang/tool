4eb49abe39e34172a84036df640b9168d94e5f15
qifanyang <584874132@qq.com>
2014-11-05 10:55:26 +0800

    blog 存库

--- tool/src/test/java/base/test/ModTest.java
+++ tool/src/test/java/base/test/ModTest.java
@@ -7,15 +7,23 @@ public class ModTest {
 	@Test
 	public void test1(){
 		int i = 1;
-		for(;i <= 10; i++){
+		int r = 0;
+		int max = 2000000000;
+		long start = System.nanoTime();
+		for(;i <= max; i++){
 			//低位全一,模拟取模
-			System.out.println(i&3);
+//			System.out.println(i&3);
+			r = i & 3;
 		}
-		System.out.println("============");
+		System.out.println(System.nanoTime() - start);
 		i = 1;
-		for(;i <= 10; i++){
-			System.out.println(i%4);
+		start = System.nanoTime();
+//		System.out.println("============");
+		for(;i <= max; i++){
+//			System.out.println(i%4);
+			r = i % 4;
 		}
+		System.out.println(System.nanoTime() - start);
 	}
 
 }
